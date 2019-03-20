dfc9a806bdd6db41bc3814e35fc61b201f5e6113
Mark Romero <markromero@Marks-MacBook-Pro.local>
2019-03-19 18:44:04 -0600

    created foundation components for app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+//CUSTOM COMPONENT
+import Header from './component/Header/Header';
+import Dashboard from './component/Dashboard/Dashboard';
+import Wizard from "./component/Wizard/Wizard";
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Header />
+        <Dashboard />
+        <Wizard />
       </div>
     );
   }
---
+++ src/component/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+//Custom component
+import House from '../House/House';
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        Dashboard
+        <House />
+      </div>
+    )
+  }
+}
